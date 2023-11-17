9d79145180fb339e0069812bf9e1f83910e93a1d
ehitale <elatihe@gmail.com>
2023-11-17 04:27:34 -0600

    'Ray tracing through free space' progress.

--- rayTracing.m
+++ rayTracing.m
@@ -1,15 +1,19 @@
-angles = [linspace(-pi/20, pi/20, 20)];
+numberOfAngles = 8;
 
-angled_0mm = zeros(4, 20);
-angled_10mm = cat(2, [10; 0; 0; 0], zeros(4, 19));
+angles = [linspace(-pi/20, pi/20, numberOfAngles)];
+
+angled_0mm = zeros(4, numberOfAngles);
+angled_10mm = zeros(4, numberOfAngles);
 
 angled_0mm(2, :) = angles; % x1 = 0, theta_x_1 = -pi/20 to pi/20, y1 = 0, theta_y_1 = 0;
 angled_10mm(2, :) = angles;
 
-ray_1 = zeros(4, 100);
-ray_2 = zeros(4, 100);
+angled_10mm(1, :) = 10;
 
-d = 0.2; % millimeters
+ray_0mm = zeros(4, 100);
+ray_10mm = zeros(4, 100);
+
+d = 0.1; % millimeters
 
 propogation = [
     1, d, 0, 0;
@@ -18,16 +22,22 @@ propogation = [
     0, 0, 0, 1
     ]; % d represents the distance propogated. 
 
-for numberOfAngles = 1:20
+figure;
+
+for numberOfAngles = 1:numberOfAngles
     for numberOfProps = 1:100
-        ray_1(:, c) = (propogation^numberOfProps) * angled_0mm(:, numberOfAngles);
+        ray_0mm(:, numberOfProps) = (propogation^numberOfProps) * angled_0mm(:, numberOfAngles);
     end
-    % plot something, and move on to the next angle.
+    plot(linspace(0, d, 100), ray_0mm(1, :), "r");
+    hold on;
 end
 
-% for c = 1:100
-%     ray_2(:, c) = (propogation^c) * ;
-% end
+for numberOfAngles = 1:numberOfAngles
+    for numberOfProps = 1:100
+        ray_10mm(:, numberOfProps) = (propogation^numberOfProps) * angled_10mm(:, numberOfAngles);
+    end
+    plot(linspace(0, d, 100), ray_10mm(1, :), "b");
+    hold on;
+end
 
-% plot(linspace(0, d, 100), ray_1);
-% plot(linspace(0, d, 100), ray_2);
+hold off; 
