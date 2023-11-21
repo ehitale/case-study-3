5dd559d438a67cd44a50fd66671f7e6395f220f3
ehitale <elatihe@gmail.com>
2023-11-21 15:19:00 -0600

    incomplete 'lfImage.m'

--- lfImage.m
+++ lfImage.m
@@ -1,8 +1,54 @@
 load("lightField.mat") % rays is in meters
 
 figure;
-[img_initial, x, y] = rays2img(rays(1, :), rays(3, :), .005, 200);
+histogram(rays(1, :));
+title "x"
+
+brookings = rays(:, (rays(1, :) <= 0.01) & rays(1, :) >= -0.049);
+% brookings(1, :) = brookings(1, :) + 83;
+avocado = rays(:, (rays(1, :) <= 0.01 & rays(1, :) >= -0.01));
+logo = rays(:, (rays(1, :) >= 0.03));
+
+% 'brookings', 'avocado', and 'logo' are all logically indexed portions
+% of rays. 
+
+figure;
+[img_initial, ~, ~] = rays2img(logo(1, :), logo(3, :), .25, 1000);
 % A 'sharper' image cannot be obtained by changing the sensor width, or the amount of pixels alone.   
+% Do something to img_initial so that you just have the lit up values for 
+
+rowCount = 0;
+rowIndices = zeros(1, size(img_initial, 1));
+columnCount = 0;
+
+for i = 1:size(img_initial, 1)
+    if (sum(img_initial(i, :) ~= 0))
+        if (rowCount < sum(img_initial(i, :) ~= 0))
+            rowCount = sum(img_initial(i, :) ~= 0);
+        end
+    end
+end
+
+for j = 1:size(img_initial, 2) 
+    if (sum(img_initial(:, j) ~= 0))
+        if (columnCount < sum(img_initial(:, j) ~= 0))
+            columnCount = sum(img_initial(:, j) ~= 0);
+        end
+    end
+end
+
+fittedImage = zeros(rowCount, columnCount);
+
+for i = 1:rowCount
+    for j = 1:size(img_initial, 1)
+        if (sum(img_initial(j, :) ~= 0))
+            fittedImage(1, :) = img_initial(j, :)
+        end
+    end
+end
+
+% The code above is supposed to index the initial image with the parameters
+% defined by the fitted image. 
 
 imshow(img_initial);
 title("initial");
@@ -10,20 +56,20 @@ hold off;
 
 % I can't really make out what's this is supposed to be a picture of. I see black and white splotches, but other than that it's unclear. 
 
-prop_2 = [ 
-    1, d_1, 0, 0;
-    0, 1, 0, 0;
-    0, 0, 1, d_1;
-    0, 0, 0, 1
-    ]; % 'd' is that of rayTracing.m
+% prop_2 = [ 
+%     1, d_1, 0, 0;
+%     0, 1, 0, 0;
+%     0, 0, 1, d_1;
+%     0, 0, 0, 1
+%     ]; % 'd' is that of rayTracing.m
 
-figure;
-propogatedRays = prop_2 * rays;
-
-[propImg, propX, propY] = rays2img(propogatedRays(1, :), propogatedRays(3, :), .005, 200);
-
-imshow(propImg)
-title("propImg")
+% figure;
+% propogatedRays = prop_2 * rays;
+% 
+% [propImg, propX, propY] = rays2img(propogatedRays(1, :), propogatedRays(3, :), .005, 200);
+% 
+% imshow(propImg)
+% title("propImg")
 
 % There doens't appear to be a value of d that generates a clear image. 
 
@@ -33,7 +79,7 @@ title("propImg")
 % not being represented in the image because they escaped the sensor. 
 
 %% Image System
-d_1_var = 5; % 10 millimeters, 1 cm.
+% d_1_var = 5; % 10 millimeters, 1 cm.
 % d_2_var = 0.01; % 10 millimeters, 1 cm.
 
 % Example matrices, not currently in use.
@@ -59,8 +105,8 @@ d_1_var = 5; % 10 millimeters, 1 cm.
 %     ];
 
 % figure;
-% % for d_1_var = 0.01:0.01:0.1
-%     for d_2_var = 0.5:0.01:1
+% % for d_1_var = 0.02:0.01:10
+%     for d_2_var = 0.02:0.01:10
 % 
 %         m_prop_1 = [
 %             1, d_1_var, 0, 0;
@@ -128,9 +174,102 @@ final_prop_2 = [
 % d_2 = 1 meters, f = 5/6 meters, assuming that d_1 is 5 meters.
 
 imageSystem = final_prop_2 * (final_lens * final_prop_1);
-image = imageSystem * rays;
+image = 2 * imageSystem * rays;
 [img, x, y] = rays2img(image(1, :), image(3, :), .005, 800);
 imshow(img);
+hold off;
 
 % The image above appears to be depicting an avocado reclining in a chair,
 % perhaps speaking to a therapist.
+
+%% Locating three images - Competition
+% figure;
+% 
+% avo_prop_1 = [
+%         1, 5, 0, 0;
+%         0, 1, 0, 0;
+%         0, 0, 1, 5;
+%         0, 0, 0, 1
+%     ];
+% 
+% f_final = (1/1 + 1/5)^(-1);
+% 
+% avo_lens = [
+%         1, 0, 0, 0;
+%         -1/f_final, 1, 0, 0;
+%         0, 0, 1, 0;
+%         0, 0, -1/f_final, 1
+%     ];
+% 
+% avo_prop_2 = [
+%         1, 1, 0, 0;
+%         0, 1, 0, 0;
+%         0, 0, 1, 1;
+%         0, 0, 0, 1
+%     ];
+% 
+% compImageSystem = comp_prop_2 * (comp_lens * comp_prop_1);
+% compImage = compImageSystem * rays;
+% 
+% [img, x, y] = rays2img(compImage(1, :), compImage(3, :), .04, 1500);
+% imshow(img(floor(end/3):floor(2*end/3), (1:floor(end/3))));
+% figure;
+% 
+% brook_prop_1 = [
+%         1, 5, 0, 0;
+%         0, 1, 0, 0;
+%         0, 0, 1, 5;
+%         0, 0, 0, 1
+%     ];
+% 
+% f_final = (1/1 + 1/5)^(-1);
+% 
+% brook_lens = [
+%         1, 0, 0, 0;
+%         -1/f_final, 1, 0, 0;
+%         0, 0, 1, 0;
+%         0, 0, -1/f_final, 1
+%     ];
+% 
+% brook_prop_2 = [
+%         1, 1, 0, 0;
+%         0, 1, 0, 0;
+%         0, 0, 1, 1;
+%         0, 0, 0, 1
+%     ];
+% 
+% compImageSystem = comp_prop_2 * (comp_lens * comp_prop_1);
+% compImage = compImageSystem * rays;
+% 
+% [img, x, y] = rays2img(compImage(1, :), compImage(3, :), .04, 1500);
+% imshow(img(floor(end/3):floor(2*end/3), (1:floor(end/3))));
+% figure;
+% 
+% washu_prop_1 = [
+%         1, 5, 0, 0;
+%         0, 1, 0, 0;
+%         0, 0, 1, 5;
+%         0, 0, 0, 1
+%     ];
+% 
+% f_final = (1/1 + 1/5)^(-1);
+% 
+% washu_lens = [
+%         1, 0, 0, 0;
+%         -1/f_final, 1, 0, 0;
+%         0, 0, 1, 0;
+%         0, 0, -1/f_final, 1
+%     ];
+% 
+% washu_prop_2 = [
+%         1, 1, 0, 0;
+%         0, 1, 0, 0;
+%         0, 0, 1, 1;
+%         0, 0, 0, 1
+%     ];
+% 
+% compImageSystem = comp_prop_2 * (comp_lens * comp_prop_1);
+% compImage = compImageSystem * rays;
+% 
+% [img, x, y] = rays2img(compImage(1, :), compImage(3, :), .04, 1500);
+% imshow(img(floor(end/3):floor(2*end/3), (1:floor(end/3))));
